56b85888763b1de9bfbfb67d2e84aeb1aded9a46
schweiny-08 <andrew.magri@outlook.com>
2020-09-08 17:00:56 +0200

    Added Heart sprites for ui

---
+++ DooDTake2/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour{
+    public Sprite fullHeart;
+    public Sprite heartThreeFour;
+    public Sprite heartHalf;
+    public Sprite heartQuater;
+
+    public Image[] hearts;
+
+    private int maxHealth, maxHealthTemp;
+    private int health, maxHealthHearts;
+
+    private PlayerController pc;
+    private GameMaster gm;
+
+    void Start() {
+        pc = GetComponent<PlayerController>();
+        gm = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+
+        maxHealth = gm.GetMaxHealth();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateMaxHealth() {
+        maxHealth += 4;
+    }
+}
